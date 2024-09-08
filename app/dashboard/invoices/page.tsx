4185316dd88379efa1421db45c673020a71809a6
orpheus6678 <66983935+orpheus6678@users.noreply.github.com>
2024-09-08 22:50:49 +0600

    Implemented authentication with NextAuth.js

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,8 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
+export const metadata = { title: 'Invoices' };
+
 export default async function Page(
   { searchParams }: { searchParams?: { query?: string, page?: string } }
 ) {
